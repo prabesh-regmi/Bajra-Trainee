129203977d837c44b0374b21332ed1e365cf9a89
prabesh-regmi-bajra <prabesh.regmi@bajratechnologies.com>
2022-11-03 12:49:13 +0545

    Permutation of string complete

--- ps4a.py
+++ ps4a.py
@@ -23,18 +23,22 @@ def get_permutations(sequence):
     a different order than what is listed here.
     '''
 
-    pass #delete this line and replace with your code here
+    if(len(sequence)==1): return [sequence]
+    result=[]
+    for i,v in enumerate(sequence):
+        result += [v+p for p in get_permutations(sequence[:i]+sequence[i+1:])]
+    return list(set(result))
 
 if __name__ == '__main__':
 #    #EXAMPLE
-#    example_input = 'abc'
-#    print('Input:', example_input)
-#    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
-#    print('Actual Output:', get_permutations(example_input))
+    example_input = 'babc'
+    print('Input:', example_input)
+    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+    print('Actual Output:', get_permutations(example_input))
     
 #    # Put three example test cases here (for your sanity, limit your inputs
 #    to be three characters or fewer as you will have n! permutations for a 
 #    sequence of length n)
 
-    pass #delete this line and replace with your code here
+    #delete this line and replace with your code here
 
