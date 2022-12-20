2f0f3ced44772ef9c7c5bd568ef1ac797d829ef0
prabesh-regmi-bajra <prabesh.regmi@bajratechnologies.com>
2022-12-20 16:40:25 +0545

    save new todo and todo list

--- models/task.py
+++ models/task.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError
+
 
 # Class for todo task
 
@@ -12,10 +14,11 @@ class Task(models.Model):
     name = fields.Char(string="Task Name", required=True)
     priority = fields.Selection(
         [('high', 'High'), ('medium', 'Medium'), ('low', 'Low')])
-    date_time = fields.Datetime(string="Date and time")
+    date_time = fields.Date(string="Date and time")
     task_list_id = fields.Many2one(
         'todo.list',
-        string="Todo list"
+        string="Todo list", 
+        
     )
     user_id = fields.Many2one(
         'res.users', 'Current User', default=lambda self: self.env.user)
@@ -23,9 +26,18 @@ class Task(models.Model):
 
     @api.model
     def create_task(self, vals):
-        return super(Task, self).create(vals)
+        if vals["name"] and vals["priority"] and vals["date_time"] and vals["task_list_id"]:
+            task = super(Task, self).create(vals)
+            return task.read()
+        else:
+            raise UserError("Null value")
 
     # Get all Tasks
     def get_all_tasks(self):
         user_id = self.env.user.id
         return {"data": self.env['todo.task'].search([("user_id", "=", user_id)]).read()}
+    @api.model
+    def get_task_with_task_list_id(self,vals):
+        user_id = self.env.user.id
+        task_list_id = vals["id"]
+        return {"data": self.env['todo.task'].search([("user_id", "=", user_id),("task_list_id", "=", task_list_id)]).read()}
--- models/task_list.py
+++ models/task_list.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 # class of task list
 
@@ -7,14 +8,28 @@ class Task_list(models.Model):
     _name = "todo.list"
     _description = "Todo.List"
 
+    _sql_constraints = [
+
+        (
+            'unique_name', 'unique(name)',
+            'This List is already present'
+        ),
+
+    ]
+
     name = fields.Char(string='List Name', required=True)
     tasks_ids = fields.One2many('todo.task', 'task_list_id', string='Tasks')
 
     # Create new Task List
     @api.model
     def create_task_list(self, vals):
-        return super(Task_list, self).create(vals)
+        if vals["name"]:
+            list_name = super(Task_list, self).create(vals)
+            return list_name.read()
+        else:
+            raise UserError("Null value")
 
     # Get all Tasks List
+
     def get_all_tasks_list(self):
         return {"data": self.env['todo.list'].search([]).read()}
